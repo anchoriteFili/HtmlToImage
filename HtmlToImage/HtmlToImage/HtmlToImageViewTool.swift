0031394072a880dd625cfdf3515149abf440baf2
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-10-30 10:19:13 +0800

    初始化项目

---
+++ HtmlToImage/HtmlToImage/HtmlToImageViewTool.swift
@@ -0,0 +1,21 @@
+//
+//  HtmlToImageViewTool.swift
+//  HtmlToImage
+//
+//  Created by zetafin on 2018/10/30.
+//  Copyright © 2018 赵宏亚. All rights reserved.
+//
+
+import UIKit
+
+class HtmlToImageViewTool: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HtmlToImage/HtmlToImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HtmlToImage
+//
+//  Created by zetafin on 2018/10/30.
+//  Copyright © 2018 赵宏亚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
