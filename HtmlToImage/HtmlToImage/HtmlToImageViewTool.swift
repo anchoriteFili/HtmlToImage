836f446fb0549e71a9ffe8dc73f70dd966d635ac
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-10-30 11:26:58 +0800

    提交一下

--- HtmlToImage/HtmlToImage/HtmlToImageViewTool.swift
+++ HtmlToImage/HtmlToImage/HtmlToImageViewTool.swift
@@ -7,8 +7,65 @@
 //
 
 import UIKit
+import WebKit
 
 class HtmlToImageViewTool: UIView {
+    
+    // 创建webview
+    @IBOutlet weak var webview: WKWebView!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
+    
+    
+    var contentController: WKUserContentController = WKUserContentController()
+    
+    
+    static func newInstance() -> HtmlToImageViewTool?{
+        let nibView = Bundle.main.loadNibNamed("HtmlToImageViewTool", owner: nil, options: nil)
+        if let view = nibView?.first as? HtmlToImageViewTool{
+            return view
+        }
+        return nil
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        load_init()
+    }
+    
+    func load_init(){
+        
+        
+    }
+    
+    // 初始化url
+    func setUrl(url: String) {
+        
+        let myURL = URL(string: url)
+        let myRequest = URLRequest(url: myURL!)
+        webview.load(myRequest)
+    }
+    
+    // 分享按钮点击
+    @IBAction func shareButtonClick(_ sender: UIButton) {
+        
+        self.imageview.image = getImageFromView(view: webview.scrollView)
+    }
+    
+    //    将某个view 转换成图像
+    func getImageFromView(view:UIView) ->UIImage{
+        
+        UIGraphicsBeginImageContext(view.bounds.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+    
+    
+    
 
     /*
     // Only override draw() if you perform custom drawing.
--- HtmlToImage/HtmlToImage/ViewController.swift
+++ HtmlToImage/HtmlToImage/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 尺寸设置
+    let HEIGHT = UIScreen.main.bounds.size.height
+    let WIDTH = UIScreen.main.bounds.size.width
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let htmlImageView = HtmlToImageViewTool.newInstance()
+        htmlImageView?.frame = CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT)
+        
+        self.view.addSubview(htmlImageView!)
+        htmlImageView?.setUrl(url: "https://anchoritefiligod.gitee.io/blogs/2017/12/19/WKWebView%E5%BF%AB%E9%80%9F%E5%88%9B%E5%BB%BA/")
     }
 
 
